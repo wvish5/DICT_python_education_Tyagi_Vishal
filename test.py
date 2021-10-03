740a0f56d7a4447c8f48e6f5d035728798f52ac1
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-10-03 21:44:48 +0300

    ChatBot {1-st stage}

--- test.py
+++
@@ -1,8 +0,0 @@
-print("Python education")
-#new string
-print("DICT")
-print ("education")
-# new string
-(test.py added to feature_branch with change on string#2)
-(test.py added to feature_branch with change on string#3)
-(test.py added to feature_branch with change on string #1)
