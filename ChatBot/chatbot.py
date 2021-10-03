a5e9059ff0bfa761a3d850b07091ad4562dc6175
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-10-03 18:43:27 +0300

    test.py added to feature_branch with change on string#3

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,16 @@
+ "Hello! My name is "ChatBot"
+ "I was created in "2021"
+ "Please,remind me your name"
+> Vishal
+"What a great name you have, Vishal!"
+age = (remainder3 * 70 + remainder5* 21 + remainder7 * 15) % 105
+Let me guess your age.
+ Enter remainders of dividing your age by 3,5 and 7.
+ Your age is {your_age}; that's a good time to start programming!
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 print("Python education")
-
+#new string
+print("DICT")
+print ("education")
+# new string
+(test.py added to feature_branch with change on string#2)
+(test.py added to feature_branch with change on string#3)
+(test.py added to feature_branch with change on string #1)
