03e4fb1f7f80f6a0045ce8290414fac4f12148e5
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-10-04 10:08:38 +0300

    ChatBot {1-st} stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,16 +1,3 @@
- "Hello! My name is "ChatBot"
- "I was created in "2021"
- "Please,remind me your name"
-> Vishal
-"What a great name you have, Vishal!"
-age = (remainder3 * 70 + remainder5* 21 + remainder7 * 15) % 105
-Let me guess your age.
- Enter remainders of dividing your age by 3,5 and 7.
- Your age is {your_age}; that's a good time to start programming!
-
-
-
-
-
-
-
+print("Hello! My name is ChatBot")
+print("I was created 'in 2021' ")
+print("Please,remind me your name")
