fa36aa928e2ecf86995975a39aae5fb078d70262
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-11-15 10:20:44 +0300

    5 stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,79 +1,33 @@
 import random
-print("HANGMAN")
-print("The game will be available soon!")
-word = ""
-letter = ""
-a = input()
-def show(letter):
-    for a in word:
-        print(a)
-        for a in letter:
-            print(a, end='')
+
+def show(word, letter):
+    m = 0
+    for i in word:
+        if i in letter:
+            print(i, end='')
         else:
             print('_', end='')
-while True:
-    type = input("play or exit:")
+            m += 1
+    return m
 
-    if type == "play":
+wordlist = ['python', 'java', 'javascript', 'php']
+n = random.choice(wordlist)
+words = ''
+show(n, words)
+attempt = 8
 
-        words = ['python', 'java', 'javascript', 'php']
-        word = random.choice(words)
-        letter = 'i'
-        wrong = 8
-        Win = True
-        attempt = 8
-        while attempt > 0:
-            while True:
-                inputletter = ""
-                inputletter += input("input a letter")
-                if len(inputletter) == 1:
-                    if inputletter.islower():
-                        if wrong != 1:
-                            if inputletter not in letter:
-                                if inputletter in word:
-                                    letter += inputletter
-                                else:
-                                    print("Данной буквы нет!")
-                                    wrong -= 1
-                            for a in word:
-                                if a in letter:
-                                    Win = True
-                                else:
-                                    Win = False
-                            if Win:
-                                print("Вы победили")
-                                break
-                        else:
-                            print("Вы уже вводили эту букву!")
-                    else:
-                        print('Вы проиграли!')
-                        break
-                else:
-                    print('Введите маленькую букву!')
-            else:
-                print('Введите одну английскую букву!')
-        else:
-            print('Wrong command!')
-    elif type == 'exit':
-        break
+while attempt != 0:
+    p = input(" Guess the letter")
+    words += p
+    if p in n:
+        if sum == 0:
+            print("\nYou win!")
+            break
+    else:
+        print("You guessed the wrong letter")
+        attempt -= 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if attempt == 0:
+        print("You loss")
+    sum = show(n, words)
 
