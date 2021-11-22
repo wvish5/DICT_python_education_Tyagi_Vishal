1878ad7552595f1c18ca8d84b3b6331e39505add
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-11-22 10:39:18 +0300

    3 stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,12 +1,17 @@
-n = int(input("Write how many cups of coffee you will need:"))
-milk = 50 * n
-beans = 15 * n
-water = 250 * n
-print(f"For {n} cups of coffee you will need:")
-print(f'{water} of water')
-print(f'{milk} of milk')
-print(f'{beans} of beans')
-
-
-
+water = int(input('Write how many water the coffee machine has: '))
+beans = int(input('Write how many beans the coffee machine has: '))
+milk = int(input('Write how many milk the coffee machine has: '))
+cups = int(input("Write how many cups of coffee you will need: "))
+m = milk // 50
+b = beans // 15
+w = water // 250
+a = [m, b, w]
+n = min(a)
+if cups == n:
+    print("Yes,I can make this amount of coffee")
+else:
+    if cups > n:
+        print(f"No,I can make only {n} cups of coffee")
+    else:
+        print(f"Yes,I can make that amount of coffee (and even {n - cups} more than that")
 
