c1b2a66e341177d95c38c2bc61cc57dc9e370d4a
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-11-24 22:10:21 +0300

    4 stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,17 +1,104 @@
-water = int(input('Write how many water the coffee machine has: '))
-beans = int(input('Write how many beans the coffee machine has: '))
-milk = int(input('Write how many milk the coffee machine has: '))
-cups = int(input("Write how many cups of coffee you will need: "))
-m = milk // 50
-b = beans // 15
-w = water // 250
-a = [m, b, w]
-n = min(a)
-if cups == n:
-    print("Yes,I can make this amount of coffee")
-else:
-    if cups > n:
-        print(f"No,I can make only {n} cups of coffee")
+money = 550
+water = 1200
+milk = 540
+beans = 120
+cups = 9
+
+
+def print_state():
+    print('The coffee machine has:')
+    print(f'{water} of water')
+    print(f'{milk} of milk')
+    print(f'{beans} of coffee beans')
+    print(f'{cups} of disposable cups')
+    print(f'{money} of money')
+    print()
+
+
+def select_action() -> str:
+    return input('Write action (buy, fill, take): ')
+
+
+def select_flavor() -> int:
+    return int(input('What do you want to buy?'
+                     ' 1 - espresso, 2 - latte, 3 - cappuccino: '))
+
+
+def buy():
+    global money, water, milk, beans, cups
+
+    flavor = select_flavor()
+    if flavor == 1:  # espresso
+
+
+        money += 4
+        water -= 250
+        beans -= 16
+    elif flavor == 2:  # latte
+
+
+        money += 7
+        water -= 350
+        milk -= 75
+        beans -= 20
+    elif flavor == 3:  # cappuccino
+
+
+        money += 6
+        water -= 200
+        milk -= 100
+        beans -= 12
     else:
-        print(f"Yes,I can make that amount of coffee (and even {n - cups} more than that")
+        print(f'Unknown flavor {flavor}')
+
+    cups -= 1
+
+
+def fill():
+    global water, milk, beans, cups
+
+    water += int(input('Write how many ml of water do you want to add: '))
+    milk += int(input('Write how many ml of milk do you want to add: '))
+    beans += int(input('Write how many grams of coffee beans'
+                       ' do you want to add: '))
+    cups += int(input('Write how many disposable cups of coffee'
+                      ' do you want to add: '))
+
+
+def take():
+    global money
+
+    print(f'I gave you ${money}')
+    money = 0
+
+
+def main():
+    print_state()
+
+    action = select_action()
+
+    if action == 'buy':
+        buy()
+    elif action == 'fill':
+        fill()
+    elif action == 'take':
+        take()
+    else:
+        print(f'Unknown command {action}')
+
+    print()
+    print_state()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
