948a051d379a390a0dc8439cb7383f066da0ae80
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-11-28 14:34:33 +0300

    2 stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,3 +1,6 @@
 print('X O X')
 print('O X O')
 print('X X O')
+x = (list(input()))
+board = '---------\n| ' + x[0] + ' ' + x[1] + ' ' + x[2] + ' | \n| ' + x[3] + ' ' + x[4] + ' ' + x[5] + ' | \n| ' + x[6] + ' ' + x[7] + ' ' + x[8] + ' | \n---------'
+print(board)
