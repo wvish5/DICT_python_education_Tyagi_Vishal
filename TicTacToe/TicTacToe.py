f601ecdb1cdd7ee4724146afd91f94f35d9c600e
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-11-28 14:38:42 +0300

    5 stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,37 +1,30 @@
-user_input = list(input("Enter cells: "))
-matrix = []
-k = 0
-def show():
-    global matrix
-    print("---------")
-    for i in range(0, 3):
-        print("|", end="")
-        for j in range(0, 3):
-            print(" " + matrix[i][j], end="")
-        print(" |")
-    print("---------")
-
-for i in range (0,3):
-    matrix.append([])
-    for j in range (0,3):
-        matrix[i].append(user_input[k])
-        k += 1
-
-show()
-
-while True:
-    x, y = input("Enter the coordinates: ").split()
-    try:
-        x = int(x)
-        y = int(y)
-        if x < 1 or y > 3 or x < 1 or y > 3:
-            print("Coordinates should be from 1 to 3!")
-        else:
-            if matrix[abs(y-3)][abs(x-1)] == "_":
-                matrix[abs(y-3)][abs(x-1)] = "X"
-                show()
-                break
-            else:
-                print("This cell is occupied! Choose another one!")
-    except:
+a = list(" " * 9)
+step = 0
+def printgame():
+    print('-' * 9)
+    for i in range(3):
+        print("| {} {} {} |".format(a[0 + 3 * i], a[1 + 3 * i], a[2 + 3 * i]))
+    print('-' * 9)
+while " " in a:
+    playx, playy = input("Enter the coordinates:").split()
+    if (int(playx) or int(playy)) not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
         print("You should enter numbers!")
+    elif (int(playx) or int(playy)) not in [1, 2, 3]:
+        print("Coordinates should be from 1 to 3!")
+    elif a[(int(playx) - 1) + abs(int(playy) - 3) * 3] != " ":
+        print("This cell is occupied! Choose another one!")
+    else:
+        if step == 0:
+            a[(int(playx) - 1) + abs(int(playy) - 3) * 3] = "X"
+            step = 1
+        elif step == 1:
+            a[(int(playx) - 1) + abs(int(playy) - 3) * 3] = "O"
+            step = 0
+        printgame()
+        rule = [a[:3], a[3:6], a[6:], a[0:9:3], a[1:9:3], a[2:9:3], a[0:9:4], a[2:7:2]]
+        if ['X', 'X', 'X'] in rule:
+            print("X wins")
+        elif ['O', 'O', 'O'] in rule:
+            print("O wins")
+        elif a.count(" ") == 0:
+            print("Draw")
