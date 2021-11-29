bb3b47d061a1148c0c20f218f85b774e1a7ad7ba
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-11-29 10:33:37 +0300

    1 stage

---
+++ CreditCalculator/CreditCalculator.py
@@ -0,0 +1,19 @@
+pr = ""
+lp = ""
+pm = ""
+pp = ""
+m = ""
+payment = pp / m
+print(f'{pm}=', pm)
+lp = pp - (f'{pr} - 1) * pm')
+print('lp=', lp)
+print("Loan principal: 1000")
+print("Month 1: repaid 250")
+print("Month 2: repaid 250")
+print("Month 3: repaid 500")
+print("The loan has been repaid!")
+input("Enter the loan principal:")
+print("What do you want to calculate?")
+input("type 'm' for number of monthly payments,")
+input("type 'p' for the monthly payment: ")
+input("Enter the monthly payment: ")
