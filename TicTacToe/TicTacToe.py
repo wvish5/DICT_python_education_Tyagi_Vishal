4bf2c5f131ca6954f81dab0df7beb89417458232
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-12-13 08:35:22 +0300

    5-th stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,79 +1,31 @@
-a = input("Enter cells: ").replace("_", " ")
-a = list(a)
-
-
-def game_grid():
-    print("---------")
-    print("|", a[0], a[1], a[2], "|")
-    print("|", a[3], a[4], a[5], "|")
-    print("|", a[6], a[7], a[8], "|")
-    print("---------")
-
-
-def change():
-    global a, q, q1, q2
-    q = input("Enter the coordinates: ").split(" ")
-    q1 = q[0]
-    q2 = q[1]
-    def check(q1, q2):
-        global pos
-        if q1 == "1" and q2 == "1":
-            pos = a[0]
-            return pos
-        elif q1 == "1" and q2 == "2":
-            pos = a[1]
-            return pos
-        elif q1 == "1" and q2 == "3":
-            pos = a[2]
-            return pos
-        elif q1 == "2" and q2 == "1":
-            pos = a[3]
-            return pos
-        elif q1 == "2" and q2 == "2":
-            pos = a[4]
-            return pos
-        elif q1 == "2" and q2 == "3":
-            pos = a[5]
-            return pos
-        elif q1 == "3" and q2 == "1":
-            pos = a[6]
-            return pos
-        elif q1 == "3" and q2 == "2":
-            pos = a[7]
-            return pos
-        elif q1 == "3" and q2 == "3":
-            pos = a[8]
-            return pos
-    if check(q1, q2) != " ":
-        print("This cell is occupied! Choose another one!")
-        change()
-    elif q1 not in "0123456789" or q2 not in "0123456789":
+a = list(" " * 9)
+step = 0
+def printgame():
+    print('-' * 9)
+    for i in range(3):
+        print("| {} {} {} |".format(a[0 + 3 * i], a[1 + 3 * i], a[2 + 3 * i]))
+    print('-' * 9)
+while " " in a:
+    playx, playy = input("Enter the coordinates:").split()
+    if (int(playx) or int(playy)) not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
         print("You should enter numbers!")
-        change()
-    elif q1 not in "123" or q2 not in "123":
+    elif (int(playx) or int(playy)) not in [1, 2, 3]:
         print("Coordinates should be from 1 to 3!")
-        change()
+    elif a[(int(playx) - 1) + abs(int(playy) - 3) * 3] != " ":
+        print("This cell is occupied! Choose another one!")
     else:
-        if q1 == "1" and q2 == "1":
-            a[0] = "X"
-        elif q1 == "1" and q2 == "2":
-            a[1] = "X"
-        elif q1 == "1" and q2 == "3":
-            a[2] = "X"
-        elif q1 == "2" and q2 == "1":
-            a[3] = "X"
-        elif q1 == "2" and q2 == "2":
-            a[4] = "X"
-        elif q1 == "2" and q2 == "3":
-            a[5] = "X"
-        elif q1 == "3" and q2 == "1":
-            a[6] = "X"
-        elif q1 == "3" and q2 == "2":
-            a[7] = "X"
-        elif q1 == "3" and q2 == "3":
-            a[8] = "X"
-        game_grid()
+        if step == 0:
+            a[(int(playx) - 1) + abs(int(playy) - 3) * 3] = "X"
+            step = 1
+        elif step == 1:
+            a[(int(playx) - 1) + abs(int(playy) - 3) * 3] = "O"
+            step = 0
+        printgame()
+        rule = [a[:3], a[3:6], a[6:], a[0:9:3], a[1:9:3], a[2:9:3], a[0:9:4], a[2:7:2]]
+        if ['X', 'X', 'X'] in rule:
+            print("X wins")
+        elif ['O', 'O', 'O'] in rule:
+            print("O wins")
+        elif a.count(" ") == 0:
+            print("Draw")
 
-
-game_grid()
-change()
