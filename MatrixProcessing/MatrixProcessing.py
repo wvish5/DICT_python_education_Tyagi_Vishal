4ad3e7912c634245b3b5346cd862b4dda27f684a
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-12-20 17:11:01 +0300

    2-nd stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -9,34 +9,17 @@ def matrixA():
         q = input().split(" ")
         matrix_A[i] = [int(j) for j in q]
 
-def matrixB():
-    global matrix_B, n2, m2
-    q = input().split(" ")
-    n2, m2, = int(q[0]), int(q[1])
-    matrix_B = []
-    for t in range(n2):
-        matrix_B.append([])
-    for i in range(n2):
-        q = input().split(" ")
-        matrix_B[i] = [int(j) for j in q]
+def multiplication():
+    c = int(input())
+    for i in range(n1):
+        for j in range(m1):
+            matrix_A[i][j] *= c
 
-def check():
-    if n1==n2 and m1==m2:
-        matrixSum()
-    else:
-        print("ERROR")
-        quit()
-
-def matrixSum():
-    for i in range(n2):
-        for j in range(m2):
-            matrix_A[i][j] += matrix_B[i][j]
 
 def output():
     for i in range(n1):
         print(*matrix_A[i], sep=" ")
 
 matrixA()
-matrixB()
-check()
+multiplication()
 output()
