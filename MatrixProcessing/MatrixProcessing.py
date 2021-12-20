1715f2c7a8a20c08f25673c0ba39eb50ea6eba86
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-12-20 17:11:46 +0300

    3-rd stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,25 +1,89 @@
-def matrixA():
-    global matrix_A, n1, m1
-    q = input().split(" ")
-    n1, m1, = int(q[0]), int(q[1])
-    matrix_A = []
-    for t in range(n1):
-        matrix_A.append([])
-    for i in range(n1):
-        q = input().split(" ")
-        matrix_A[i] = [int(j) for j in q]
-
-def multiplication():
-    c = int(input())
-    for i in range(n1):
-        for j in range(m1):
-            matrix_A[i][j] *= c
+def menu():
+    print("1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n0. Exit")
+    choice = int(input("Your choice: "))
+    if choice == 1:
+        cho_add()
+    if choice == 2:
+        cho_multiply_constant()
+    if choice == 3:
+        cho_multiply()
+    if choice == 0:
+        quit()
 
 
-def output():
-    for i in range(n1):
-        print(*matrix_A[i], sep=" ")
+def cho_add():
+    q = input("Enter size of first matrix: ").split(" ")
+    if len(q) > 1:
+        nA = int(q[0])
+        mA = int(q[1])
+    else:
+        print("The operation cannot be performed.")
+        print()
+        menu()
+    print("Enter first matrix:")
+    matrix_A = [list(map(float, input().split(" "))) for _ in range(nA)]
+    nB, mB = [int(y) for y in input("Enter size of second matrix: ").split(" ")]
+    print("Enter second matrix:")
+    matrix_B = [list(map(float, input().split(" "))) for _ in range(nB)]
 
-matrixA()
-multiplication()
-output()
+    if nA != nB and nB != mB:
+        print("The operation cannot be performed.")
+        print()
+        menu()
+    print("The result is:")
+    for i in range(nA):
+        for j in range(mA):
+            matrix_A[i][j] += matrix_B[i][j]
+    for ii in range(nA):
+        print(*matrix_A[ii], sep=" ")
+    print()
+    menu()
+
+
+def cho_multiply_constant():
+    n, m = [int(x) for x in input("Enter size of matrix: ").split(" ")]
+    print("Enter matrix:")
+    matrix = [list(map(float, input().split(" "))) for _ in range(n)]
+    c = float(input("Enter constant: "))
+    print("The result is:")
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] *= c
+    for ii in range(n):
+        print(*matrix[ii], sep=" ")
+    print()
+    menu()
+
+
+def cho_multiply():
+    nA, mA = [int(x) for x in input("Enter size of first matrix: ").split(" ")]
+    print("Enter first matrix:")
+    m_A = [list(map(float, input().split(" "))) for _ in range(nA)]
+
+    nB, mB = [int(x) for x in input("Enter size of second matrix: ").split(" ")]
+    print("Enter second matrix:")
+    m_B = [list(map(float, input().split(" "))) for _ in range(nB)]
+
+    if mA != nB:
+        print("The operation cannot be performed.")
+        print()
+        menu()
+    matrix = []
+    for w in range(nB):
+        matrix.append([])
+        for ww in range(mB):
+            matrix[w].append(0)
+
+    for i in range(nA):  # i=0  j=1  k=0
+        for j in range(mB):
+            for k in range(nB):
+                matrix[i][j] += m_A[i][k] * m_B[k][j]
+
+    print("The result is:")
+    for ii in range(nA):
+        print(*matrix[ii], sep=" ")
+    print()
+    menu()
+
+
+menu()
