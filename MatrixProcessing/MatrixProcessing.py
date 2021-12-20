7c41ed01a0cfbc3e3705584d06935fd658a273c3
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2021-12-20 17:13:10 +0300

    4-th stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,89 +1,85 @@
-def menu():
-    print("1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n0. Exit")
-    choice = int(input("Your choice: "))
-    if choice == 1:
-        cho_add()
-    if choice == 2:
-        cho_multiply_constant()
-    if choice == 3:
-        cho_multiply()
+from numpy import matrix, rot90, fliplr, flipud
+
+
+class Matrix:
+    def __init__(self, name):
+        self.name = name
+        self.n, self.m = None, None
+        self.matrix = None
+
+    def matrix_reader(self):
+        self.n, self.m = map(int, input(f'Enter size of {self.name}matrix: ').split())
+        print(f'Enter {self.name}matrix:')
+        self.matrix = matrix([list(map(float, input().split())) for _ in range(self.n)])
+
+    def __eq__(self, other):
+        return self.n == other.n and self.m == other.m
+
+    def __add__(self, other):
+        return self.matrix + other.matrix
+
+    def mul(self, number):
+        return self.matrix * number
+
+    def __mul__(self, other):
+        return self.matrix * other.matrix
+
+    def print_matrix(self):
+        for col in self.matrix.tolist():
+            print(*col)
+
+
+while True:
+    print('1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n4. Transpose matrix\n0. Exit')
+    choice = int(input('Your choice: '))
     if choice == 0:
-        quit()
-
-
-def cho_add():
-    q = input("Enter size of first matrix: ").split(" ")
-    if len(q) > 1:
-        nA = int(q[0])
-        mA = int(q[1])
-    else:
-        print("The operation cannot be performed.")
-        print()
-        menu()
-    print("Enter first matrix:")
-    matrix_A = [list(map(float, input().split(" "))) for _ in range(nA)]
-    nB, mB = [int(y) for y in input("Enter size of second matrix: ").split(" ")]
-    print("Enter second matrix:")
-    matrix_B = [list(map(float, input().split(" "))) for _ in range(nB)]
-
-    if nA != nB and nB != mB:
-        print("The operation cannot be performed.")
-        print()
-        menu()
-    print("The result is:")
-    for i in range(nA):
-        for j in range(mA):
-            matrix_A[i][j] += matrix_B[i][j]
-    for ii in range(nA):
-        print(*matrix_A[ii], sep=" ")
-    print()
-    menu()
-
-
-def cho_multiply_constant():
-    n, m = [int(x) for x in input("Enter size of matrix: ").split(" ")]
-    print("Enter matrix:")
-    matrix = [list(map(float, input().split(" "))) for _ in range(n)]
-    c = float(input("Enter constant: "))
-    print("The result is:")
-    for i in range(n):
-        for j in range(n):
-            matrix[i][j] *= c
-    for ii in range(n):
-        print(*matrix[ii], sep=" ")
-    print()
-    menu()
-
-
-def cho_multiply():
-    nA, mA = [int(x) for x in input("Enter size of first matrix: ").split(" ")]
-    print("Enter first matrix:")
-    m_A = [list(map(float, input().split(" "))) for _ in range(nA)]
-
-    nB, mB = [int(x) for x in input("Enter size of second matrix: ").split(" ")]
-    print("Enter second matrix:")
-    m_B = [list(map(float, input().split(" "))) for _ in range(nB)]
-
-    if mA != nB:
-        print("The operation cannot be performed.")
-        print()
-        menu()
-    matrix = []
-    for w in range(nB):
-        matrix.append([])
-        for ww in range(mB):
-            matrix[w].append(0)
-
-    for i in range(nA):  # i=0  j=1  k=0
-        for j in range(mB):
-            for k in range(nB):
-                matrix[i][j] += m_A[i][k] * m_B[k][j]
-
-    print("The result is:")
-    for ii in range(nA):
-        print(*matrix[ii], sep=" ")
-    print()
-    menu()
-
-
-menu()
+        exit()
+    if choice == 1:
+        first = Matrix('first ')
+        first.matrix_reader()
+        second = Matrix('second ')
+        second.matrix_reader()
+        print('The result is:')
+        if first == second:
+            result = Matrix('')
+            result.matrix = first + second
+            result.print_matrix()
+        else:
+            print('The operation cannot be performed.')
+    if choice == 2:
+        first = Matrix('')
+        first.matrix_reader()
+        const = float(input('Enter constant: '))
+        result = Matrix('')
+        result.matrix = first.mul(const)
+        print('The result is:')
+        result.print_matrix()
+    if choice == 3:
+        first = Matrix('first ')
+        first.matrix_reader()
+        second = Matrix('second ')
+        second.matrix_reader()
+        print('The result is:')
+        if first.m == second.n:
+            result = Matrix('')
+            result.matrix = first * second
+            result.print_matrix()
+        else:
+            print('The operation cannot be performed.')
+    if choice == 4:
+        print('1. Main diagonal', '2. Side diagonal',
+              '3. Vertical line', '4. Horizontal line')
+        choice_ = int(input('Your choice: '))
+        first = Matrix('')
+        first.matrix_reader()
+        print('The result is:')
+        result = Matrix('')
+        if choice_ == 1:
+            result.matrix = first.matrix.T
+        if choice_ == 2:
+            result.matrix = rot90(rot90(first.matrix).T, 3)
+        if choice_ == 3:
+            result.matrix = fliplr(first.matrix)
+        if choice_ == 4:
+            result.matrix = flipud(first.matrix)
+        result.print_matrix()
