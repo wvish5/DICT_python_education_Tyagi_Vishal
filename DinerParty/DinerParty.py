bb3777a0d91c39c460ed300ac8f7d2581c4cd6fb
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2022-01-27 15:18:31 +0300

    4-th stage

--- DinerParty/DinerParty.py
+++ DinerParty/DinerParty.py
@@ -1,6 +1,7 @@
 from random import choice
 count = int(input("Enter the number of friends joining (including you):"))
 n = {}
+
 sh = []
 if count >= 0:
     print("Enter the name of every friend (including you), each on a new line:")
@@ -8,17 +9,26 @@ if count >= 0:
         name = input()
         n[name] = 0
         sh.append(name)
+
 else:
     print("No one is joining for the party")
 summa = int(input("Enter the total amount:"))
 certain = round(summa / count, 2)
+    print("No one is joining for the party")
+summa = int(input("Enter the total amount: "))
+fact = round(summa / count, 2)
 for name in n:
-
-    print(n)
     n[name] = certain
 sh_name = choice(sh)
 question = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No:")
 if question == "Yes":
-    print(sh_name, "is Lucky one!")
+    n[name] = fact
+sh_name = choice(sh)
+question = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No ")
+if question == "Yes" or "yes":
+    print(sh_name, "Is Lucky one!")
+    n[sh_name] = 0
+    print(n)
 else:
     print("No one is going to be lucky")
+    print("No one is going to be lucky\n", n)
