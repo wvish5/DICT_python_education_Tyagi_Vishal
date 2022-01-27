5179443900d66e9823bb4e3a15424e13820153d8
Tyagi Vishal <v.a.tiagi@student.khai.edu>
2022-01-27 14:53:35 +0300

    2-nd stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -2,9 +2,15 @@ count = int(input("Enter the number of friends joining (including you):"))
 n = {}
 if count >= 0:
     print("Enter the name of every friend (including you), each on a new line:")
-    for i in range(count):
+
+    for i in range(0, count):
         name = input()
         n[name] = 0
+
 else:
     print("No one is joining for the party")
+summa = int(input("Enter the total amount:"))
+certain = round(summa / count, 2)
+for name in n:
+    n[name] = certain
 print(n)
